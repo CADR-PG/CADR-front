b57ceea9589c795f8c6376692f7421a482248b07
Szymon Mastalerz <33292538+simon04PL@users.noreply.github.com>
2025-04-06 14:33:22 +0200

    repair lint v1

--- src/editor/Data/ObjectNames.tsx
+++ src/editor/Data/ObjectNames.tsx
@@ -13,71 +13,71 @@ import SphereController from '../Objects/SphereController';
 import TetrahedronController from '../Objects/TetrahedronController';
 
 const Objects = [
-    {
-        id: 1,
-        name: "Torus",
-        component: TorusController
-    },
-    {
-        id: 2,
-        name: "Box",
-        component: BoxController
-    },
-    {
-        id: 3,
-        name: "Capsule",
-        component: CapsuleController
-    },
-    {
-        id: 4,
-        name: "Circle",
-        component: CircleController
-    },
-    {
-        id: 4,
-        name: "Cone",
-        component: ConeController
-    },
-    {
-        id: 6,
-        name: "Cylinder",
-        component: CylinderController
-    },
-    {
-        id: 7,
-        name: "Dodecahedron",
-        component: DodecahedronController
-    },
-    {
-        id: 8,
-        name: "Extrude",
-        component: ExtrudeController
-    },
-    {
-        id: 9,
-        name: "Lathe",
-        component: LatheController
-    },
-    {
-        id: 10,
-        name: "Octahedron",
-        component: OctahedronController
-    },
-    {
-        id: 11,
-        name: "Plane",
-        component: PlaneController
-    },
-    {
-        id: 12,
-        name: "Sphere",
-        component: SphereController
-    },
-    {
-        id: 13,
-        name: "Tetrahedron",
-        component: TetrahedronController
-    }
+{
+    id: 1,
+    name: "Torus",
+    component: TorusController
+},
+{
+    id: 2,
+    name: "Box",
+    component: BoxController
+},
+{
+    id: 3,
+    name: "Capsule",
+    component: CapsuleController
+},
+{
+    id: 4,
+    name: "Circle",
+    component: CircleController
+},
+{
+    id: 4,
+    name: "Cone",
+    component: ConeController
+},
+{
+    id: 6,
+    name: "Cylinder",
+    component: CylinderController
+},
+{
+    id: 7,
+    name: "Dodecahedron",
+    component: DodecahedronController
+},
+{
+    id: 8,
+    name: "Extrude",
+    component: ExtrudeController
+},
+{
+    id: 9,
+    name: "Lathe",
+    component: LatheController
+},
+{
+    id: 10,
+    name: "Octahedron",
+    component: OctahedronController
+},
+{
+    id: 11,
+    name: "Plane",
+    component: PlaneController
+},
+{
+    id: 12,
+    name: "Sphere",
+    component: SphereController
+},
+{
+    id: 13,
+    name: "Tetrahedron",
+    component: TetrahedronController
+}
 ];
 
 export default Objects;
