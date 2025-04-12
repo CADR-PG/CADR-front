6cb8a1b371554ed9d0157b22c5722d13295cdb92
Szymon Mastalerz <33292538+simon04PL@users.noreply.github.com>
2025-04-12 15:06:24 +0200

    lint fix

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,22 @@
 import { Link } from 'react-router-dom';
 import logo from './../assets/logo.png';
 
-function NavBar () {
-    return (
-        <div className="navbar-hld">
-            <div className="navbar__logo">
-                <img className="logo" src={logo} height="40px" />
-            </div>
-            <div className="navbar__btn">
-                <Link to="/register" className="nav__link">
-                    <button className="nav__btn">Register now</button>
-                </Link>
-                <Link to="/login" className="home__link">
-                    <button className="nav__btn">Login</button>
-                </Link>
-            </div>
-        </div>
-    );
+function NavBar() {
+  return (
+    <div className="navbar-hld">
+      <div className="navbar__logo">
+        <img className="logo" src={logo} height="40px" />
+      </div>
+      <div className="navbar__btn">
+        <Link to="/register" className="nav__link">
+          <button className="nav__btn">Register now</button>
+        </Link>
+        <Link to="/login" className="home__link">
+          <button className="nav__btn">Login</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
