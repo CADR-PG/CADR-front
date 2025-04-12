04cd1c06f4543e3c9902328eb1755f24af9a4040
Szymon Mastalerz <33292538+simon04PL@users.noreply.github.com>
2025-04-12 15:04:01 +0200

    navbar on homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,20 @@
 import { Link } from 'react-router-dom';
+import NavBar from './../components/NavBar';
 
 function Home() {
   return (
-    <section className="l-section l-section--home">
-      <h1>Welcome to CADR</h1>
-      <p>Your 3D modeling and rendering tool.</p>
-      <div className="home__btn--section">
-        <Link to="/editor" className="home__link">
-          <button className="home__btn">Go to Editor</button>
-        </Link>
-        <Link to="/register" className="home__link">
-          <button className="home__btn">Register now</button>
-        </Link>
-        <Link to="/login" className="home__link">
-          <button className="home__btn">Login</button>
-        </Link>
-      </div>
-    </section>
+    <div>
+      <NavBar />
+      <section className="l-section l-section--home">
+        <h1>Welcome to CADR</h1>
+        <p>Your 3D modeling and rendering tool.</p>
+        <div className="home__btn--section">
+          <Link to="/editor" className="home__link">
+            <button className="home__btn">Go to Editor</button>
+          </Link>
+        </div>
+      </section>
+    </div>
   );
 }
 
