bf98c7825bba249d32459a889dc6eda64977cfc8
m1k53r <mikobw369@protonmail.com>
2025-04-13 20:34:38 +0200

    init

--- src/components/editor/Toolbar.tsx
+++ src/components/editor/Toolbar.tsx
@@ -1,5 +1,37 @@
-function Toolbar() {
-  return <div className="toolbar"></div>;
+import { Dispatch } from 'react';
+import EditingMode from '../../types/EditingMode';
+
+interface ToolbarProps {
+  editingMode: EditingMode;
+  selectMode: Dispatch<EditingMode>;
+}
+
+function Toolbar({ editingMode, selectMode }: ToolbarProps) {
+  return (
+    <div className="toolbar">
+      <button
+        className="toolbar-item"
+        style={{ background: editingMode === 'translate' ? 'cyan' : 'white' }}
+        onClick={() => selectMode('translate')}
+      >
+        T
+      </button>
+      <button
+        className="toolbar-item"
+        style={{ background: editingMode === 'rotate' ? 'cyan' : 'white' }}
+        onClick={() => selectMode('rotate')}
+      >
+        R
+      </button>
+      <button
+        className="toolbar-item"
+        style={{ background: editingMode === 'scale' ? 'cyan' : 'white' }}
+        onClick={() => selectMode('scale')}
+      >
+        S
+      </button>
+    </div>
+  );
 }
 
 export default Toolbar;
