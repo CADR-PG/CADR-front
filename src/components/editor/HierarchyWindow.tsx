fb55e38786b089cf3088cf0e6e68235c5ba83e5c
mickami742 <mickami742@gmail.com>
2025-05-10 13:05:07 +0200

    feat: visibility icon

--- src/components/editor/HierarchyWindow.tsx
+++ src/components/editor/HierarchyWindow.tsx
@@ -1,9 +1,20 @@
 import { useEffect, useRef } from 'react';
 import { useEditorContext } from '../../hooks/useEditorContext';
+import VisibilityOutlined  from '@mui/icons-material/VisibilityOutlined';
+import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
+import { SceneObject } from '../../types/SceneObject';
 
 function HierarchyWindow() {
   const { sceneObjects, focused, focus } = useEditorContext();
   const parentRef = useRef<HTMLDivElement>(null);
+
+  const handleClick = (object:SceneObject) => {
+    if(object.ref!.visible) {
+      object.ref!.visible = false;
+    }else {
+      object.ref!.visible = true;
+    }
+  }
 
   // TODO: make it more generic and extract it to a hook
   useEffect(() => {
@@ -31,6 +42,20 @@ function HierarchyWindow() {
           style={{ background: focused == uuid ? '#555' : '' }}
         >
           {object.name}
+          <div className="buttonContainer"
+            style={{display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-end'}}
+          >
+            <button 
+              style={{ background: 'none', border: 'none', padding: '0px', margin: '0px' }}
+              onClick={() => handleClick(object)}
+            >
+              {object.ref && object.ref!.visible ? (
+                <VisibilityOutlined style={{ color: 'white' }} />
+              ) : (
+                <VisibilityOffOutlinedIcon style={{ color: 'white' }} />
+              )}
+            </button>
+          </div>
         </div>
       ))}
     </div>
