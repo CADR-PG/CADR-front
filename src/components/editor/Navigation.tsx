56cf0a5a86912e6dcbd7ffaacadab06258cb03fb
m1k53r <mikobw369@protonmail.com>
2025-05-17 13:25:13 +0200

    save before fucking up this code

--- src/components/editor/Navigation.tsx
+++ src/components/editor/Navigation.tsx
@@ -1,4 +1,4 @@
-import { MenuItem } from '@mui/material';
+import { Button, MenuItem } from '@mui/material';
 import Objects from '../../data/ObjectNames';
 import NavigationItem from './NavigationItem';
 import { SceneObject } from '../../types/SceneObject';
@@ -6,38 +6,9 @@ import { useEditorContext } from '../../hooks/useEditorContext';
 import { downloadJSON } from '../../utils';
 import { ChangeEvent, useRef } from 'react';
 import * as THREE from 'three';
-import GenericPrimitive from '../PrimitiveController';
-import { SceneObjects } from '../../types/SceneObject';
-
-function parseScene(obj: THREE.Object3D): SceneObjects {
-  const newObjects: SceneObjects = {};
-
-  obj.traverse((o) => {
-    const mesh = o as THREE.Mesh;
-    if (!mesh.isMesh || mesh.geometry.type == 'BufferGeometry') return;
-
-    const uuid = crypto.randomUUID();
-    const object: SceneObject = {
-      name: mesh.geometry.type.replace('Geometry', ''),
-      component: () => (
-        <GenericPrimitive
-          objectUuid={uuid}
-          position={mesh.position}
-          rotation={mesh.rotation}
-          scale={mesh.scale}
-          object={mesh}
-        />
-      ),
-    };
-
-    newObjects[uuid] = object;
-  });
-
-  return newObjects;
-}
 
 function Navigation() {
-  const { sceneObjects, setSceneObjects, focus } = useEditorContext();
+  const { sceneObjects, setSceneObjects } = useEditorContext();
   const filePickerRef = useRef<HTMLInputElement>(null!);
   const loader = new THREE.ObjectLoader();
 
@@ -47,35 +18,38 @@ function Navigation() {
   };
 
   const saveScene = () => {
-    if (Object.values(sceneObjects).length == 0) return;
-
-    downloadJSON(
-      // TODO: this is quite dumb, but will work for now.
-      // if we add support for groups in the future
-      // then this shit has to be changed
-      Object.values(sceneObjects)[0].ref?.parent?.toJSON(),
-      'scene.json',
-    );
-  };
+    // this is quite dumb, but will work for now.
+    // if we add support for groups in the future
+    // then this shit has to be changed
+    const group = new THREE.Group();
+    Object.values(sceneObjects).forEach(object => group.add(object.ref!));
+    console.log(group);
+    downloadJSON(group, "scene.json");
+  }
 
   const openScene = async (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files == null) return;
-
-    focus(null);
-    const text = await e.target.files[0].text();
+    const text = await e.target.files![0].text();
     const json = JSON.parse(text);
-    loader.parse(json, (obj) => setSceneObjects(parseScene(obj)));
-  };
+    loader.parse(json,
+      function (obj) {
+        obj.traverse(o => console.log(o));
+      }
+    )
+  }
 
-  const openFilePicker = () => {
+  const openFilePicker = ()  => {
     filePickerRef.current.click();
-  };
+  }
 
   return (
     <div className="tool-bar">
       <NavigationItem label="File">
-        <MenuItem onClick={() => saveScene()}>Save</MenuItem>
-        <MenuItem onClick={() => openFilePicker()}>Open...</MenuItem>
+        <MenuItem onClick={() => saveScene()}>
+          Save
+        </MenuItem>
+        <MenuItem onClick={() => openFilePicker()}>
+          Open...
+        </MenuItem>
       </NavigationItem>
       <NavigationItem label="Shapes">
         {Objects.map((object) => (
