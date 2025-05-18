8d3e0d8b8be60b88eb0042e9646cbc30b7ec360e
m1k53r <mikobw369@protonmail.com>
2025-05-18 13:40:07 +0200

    satisfactory

--- src/components/MeshController.tsx
+++ src/components/MeshController.tsx
@@ -1,47 +1,17 @@
-import { useCallback, useState } from 'react';
-import { ThreeEvent } from '@react-three/fiber';
-import * as THREE from 'three';
-import { useEditorContext } from '../hooks/useEditorContext';
 import ControllerProps from '../types/ControllerProps';
-import { Outlines } from '@react-three/drei';
+import { useMesh } from '../hooks/useMesh';
+import { Helper, Outlines } from '@react-three/drei';
+import { BoxHelper } from 'three';
 
 function GenericMesh({ children, objectUuid, ...props }: ControllerProps) {
-  const [hovered, hover] = useState(false);
-  const [clicked, click] = useState(false);
-  const { focus, setSceneObjects } = useEditorContext();
-
-  const handleClick = (e: ThreeEvent<PointerEvent>) => {
-    e.stopPropagation();
-    click(!clicked);
-    focus(objectUuid);
-  };
-
-  const handlePointerOver = (e: ThreeEvent<PointerEvent>) => {
-    e.stopPropagation();
-    hover(true);
-  };
-
-  const handlePointerOut = (e: ThreeEvent<PointerEvent>) => {
-    e.stopPropagation();
-    hover(false);
-  };
-
-  const handleRef = useCallback(
-    (node: THREE.Mesh) => {
-      // this piece of shit took like an hour of my life
-      // because it fires when the ref gets set to null
-      if (!node) return;
-
-      setSceneObjects((prevObjects) => ({
-        ...prevObjects,
-        [objectUuid]: {
-          ...prevObjects[objectUuid],
-          ref: node,
-        },
-      }));
-    },
-    [objectUuid, setSceneObjects],
-  );
+  const {
+    focused,
+    hovered,
+    handleRef,
+    handleClick,
+    handlePointerOver,
+    handlePointerOut,
+  } = useMesh(objectUuid);
 
   return (
     <mesh
@@ -52,7 +22,10 @@ function GenericMesh({ children, objectUuid, ...props }: ControllerProps) {
       onPointerOut={handlePointerOut}
     >
       {children}
-      <Outlines thickness={hovered ? 2 : 0} color="white" />
+      {hovered ? <Outlines thickness={2} color="yellow" /> : null}
+      {objectUuid == focused ? (
+        <Helper type={BoxHelper} args={['yellow']} />
+      ) : null}
     </mesh>
   );
 }
