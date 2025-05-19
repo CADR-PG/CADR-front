8cf4ad73c59b03f88799e88deb6060f489de7944
m1k53r <mikobw369@protonmail.com>
2025-05-19 11:29:28 +0200

    save

---
+++ src/components/HighlightHelper.tsx
@@ -0,0 +1,21 @@
+import { Helper, Outlines } from "@react-three/drei";
+import { BoxHelper } from "three";
+
+interface HighlightHelperProps {
+  objectUuid: string;
+  focused: string | null;
+  hovered: boolean;
+}
+
+function HighlightHelper({objectUuid, focused, hovered}: HighlightHelperProps) {
+  return (
+    <>
+      {hovered ? <Outlines thickness={2} color="yellow" /> : null}
+      {objectUuid == focused ? (
+        <Helper type={BoxHelper} args={['yellow']} />
+      ) : null}
+    </>
+  );
+}
+
+export default HighlightHelper;
