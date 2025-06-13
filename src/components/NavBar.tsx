e0194b183f7072db6b657cf9eb290ae503872d5f
Szymon Mastalerz <33292538+simon04PL@users.noreply.github.com>
2025-06-13 11:30:18 +0200

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,33 +1,31 @@
 import { Link } from 'react-router-dom';
 import logo from './../assets/logo.png';
-import { useEffect, useState } from 'react';
+import useUserStore from '../stores/useUserStore';
 
 function NavBar() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const cookies = document.cookie.split(';').map((cookie) => cookie.trim());
-    const tokenCookie = cookies.find((cookie) =>
-      cookie.startsWith('token=cadr_access_token'),
-    );
-    setIsLoggedIn(!!tokenCookie);
-  }, []);
+  const { isLoggedIn } = useUserStore();
 
   return (
     <div className="navbar-hld">
       <Link to="/" className="navbar__logo">
-        <img className="logo" src={logo} height="40px" alt="Logo" />
+        <img className="logo" src={logo} height="40px" />
       </Link>
-      {!isLoggedIn && (
-        <div className="navbar__btn">
-          <Link to="/register" className="nav__link">
-            <button className="nav__btn">Register now</button>
+      <div className="navbar__btn">
+        {isLoggedIn ? (
+          <Link to="/logout" className="nav__link">
+            <button className="nav__btn">Logout</button>
           </Link>
-          <Link to="/login" className="home__link">
-            <button className="nav__btn">Login</button>
-          </Link>
-        </div>
-      )}
+        ) : (
+          <>
+            <Link to="/register" className="nav__link">
+              <button className="nav__btn">Register now</button>
+            </Link>
+            <Link to="/login" className="home__link">
+              <button className="nav__btn">Login</button>
+            </Link>
+          </>
+        )}
+      </div>
     </div>
   );
 }
