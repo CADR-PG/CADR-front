131d588ca3c6e87ac257a5653d989d1b7ea516be
Szymon Mastalerz <33292538+simon04PL@users.noreply.github.com>
2025-06-13 11:27:22 +0200

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import logo from './../assets/logo.png';
-<<<<<<< create-env
 import { useEffect, useState } from 'react';
 
 function NavBar() {
@@ -13,19 +12,12 @@ function NavBar() {
     );
     setIsLoggedIn(!!tokenCookie);
   }, []);
-=======
-import useUserStore from '../stores/useUserStore';
-
-function NavBar() {
-  const { isLoggedIn } = useUserStore();
->>>>>>> main
 
   return (
     <div className="navbar-hld">
       <Link to="/" className="navbar__logo">
         <img className="logo" src={logo} height="40px" alt="Logo" />
       </Link>
-<<<<<<< create-env
       {!isLoggedIn && (
         <div className="navbar__btn">
           <Link to="/register" className="nav__link">
@@ -36,24 +28,6 @@ function NavBar() {
           </Link>
         </div>
       )}
-=======
-      <div className="navbar__btn">
-        {isLoggedIn ? (
-          <Link to="/logout" className="nav__link">
-            <button className="nav__btn">Logout</button>
-          </Link>
-        ) : (
-          <>
-            <Link to="/register" className="nav__link">
-              <button className="nav__btn">Register now</button>
-            </Link>
-            <Link to="/login" className="home__link">
-              <button className="nav__btn">Login</button>
-            </Link>
-          </>
-        )}
-      </div>
->>>>>>> main
     </div>
   );
 }
