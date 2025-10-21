c9e527d7d36a9036912e14250a2a754d8420bd18
m1k53r <mikobw369@protonmail.com>
2025-10-21 15:51:37 +0200

    progress save with different types

--- src/types/Component.ts
+++ src/types/Component.ts
@@ -1,7 +1,10 @@
-export type CUID = string;
-
-export interface Component { }
-
-export interface Components {
-  [cuid: CUID]: Component;
+export abstract class Component {
+  static name: string;
 }
+
+// TODO: Since we don't know the potential arguments for component's constructor,
+// let's just use `any` type. At the point of writing this, I have no idea if
+// we ever even use constructors with arguments, so in the future we could
+// possibly remove this eslint w/a.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type ComponentType<T extends Component> = new (...args: any[]) => T;
