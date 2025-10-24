93299099c11252b88b7634886af880efdbff0283
m1k53r <mikobw369@protonmail.com>
2025-10-24 21:27:18 +0200

    saving before doing something dumb

--- src/engine/SystemManager.ts
+++ src/engine/SystemManager.ts
@@ -1,9 +1,14 @@
-import { System } from '../types/System';
+import { System, SystemJSX } from '../types/System';
 
 export class SystemManager {
   registerSystem<T extends System>(system: T) {
     this.systems.push(system);
   }
 
+  registerSystemJSX<T extends SystemJSX>(system: T) {
+    this.systemsJsx.push(system);
+  }
+
   systems: System[] = [];
+  systemsJsx: SystemJSX[] = [];
 }
--- src/types/Material.ts
+++ src/types/Material.ts
@@ -1,10 +1,33 @@
+import { JSX } from 'react';
+import BasicMaterial from '../engine/components/BasicMaterial';
 import { Component } from './Component';
 
-// export default interface Material extends Component {
-//   type: 'Material';
-//   opacity: number;
-// }
+export interface BasicMaterialData {
+  color: [number, number, number];
+}
+
+export interface PhongMaterialData {
+  emissive: [number, number, number];
+}
+
+type MaterialType = 'basic' | 'phong' | 'toon';
+type MaterialData = BasicMaterialData | PhongMaterialData;
 
 export default class Material implements Component {
+  constructor(type: MaterialType, data: MaterialData) {
+    this.type = type;
+    this.data = data;
+  }
+
   name = 'Material';
+  type: MaterialType;
+  data: MaterialData;
 }
+
+interface MaterialToComponent {
+  [name: string]: (...args: any[]) => JSX.Element;
+}
+
+export const mapMaterialToComponent: MaterialToComponent = {
+  basic: BasicMaterial,
+};
