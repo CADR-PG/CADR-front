c5aa639efe078f1cfb61f037cccbb6224f42a9f5
Szymek615 <115280551+Szymek615@users.noreply.github.com>
2025-11-15 13:18:26 +0100

    Feature/auto save (#137)
    Co-authored-by: Szymon <s200809@student.pg.edu.pl>

--- src/components/editor/FileNavigationItem.tsx
+++ src/components/editor/FileNavigationItem.tsx
@@ -3,12 +3,13 @@ import NavigationItem from './NavigationItem';
 import { SceneObject } from '../../types/SceneObject';
 import { useEditorContext } from '../../hooks/useEditorContext';
 import { parseScene } from '../../utils';
-import { ChangeEvent, useCallback, useRef } from 'react';
+import { ChangeEvent, useCallback, useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import GenericGLTF from '../GLTFController';
 import useSaveScene from '../../hooks/useSaveScene';
 import { useParams } from 'react-router-dom';
 import SnackbarProvider from '../SnackbarProvider';
+import { useState } from 'react';
 
 function FileNavigationItem() {
   const { sceneObjects, setSceneObjects, focus } = useEditorContext();
@@ -17,25 +18,41 @@ function FileNavigationItem() {
   const filePickerRef = useRef<(HTMLInputElement | null)[]>([]);
   const loader = new THREE.ObjectLoader();
 
-  const saveScene = () => {
-    if (Object.values(sceneObjects).length == 0) return;
+  const [secondsLeft, setSecondsLeft] = useState(60);
 
-    /*
-    console.log(Object.values(sceneObjects)[0].ref?.parent?.toJSON());
-    downloadJSON(
-      // TODO: this is quite dumb, but will work for now.
-      // if we add support for groups in the future
-      // then this shit has to be changed
-      Object.values(sceneObjects)[0].ref?.parent?.toJSON(),
-      'scene.json',
-    );
-     */
+  const saveScene = useCallback(() => {
+    if (Object.values(sceneObjects).length == 0) return;
     const scene = Object.values(sceneObjects)[0].ref?.parent?.toJSON();
     mutate({
       id: uuid ? uuid : '',
       data: scene!,
     });
-  };
+  }, [sceneObjects, mutate, uuid]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSecondsLeft((prev) => {
+        if (prev <= 1) {
+          saveScene();
+          return 60;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [saveScene]);
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if ((e.ctrlKey || e.metaKey) && e.key === 's') {
+        e.preventDefault();
+        saveScene();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [saveScene]);
 
   const openScene = async (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files == null) return;
@@ -77,6 +94,7 @@ function FileNavigationItem() {
         <MenuItem onClick={() => filePickerRef.current[1]?.click()}>
           Import...
         </MenuItem>
+        <MenuItem disabled>Autosave in: {secondsLeft}s</MenuItem>
       </NavigationItem>
       <input ref={setRef(0)} type="file" onChange={openScene} hidden />
       <input ref={setRef(1)} type="file" onChange={openModel} hidden />
