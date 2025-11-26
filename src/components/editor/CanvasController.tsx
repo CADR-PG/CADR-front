ee1c255887cf2fde6f12965edf930bf718afe611
Szymon <s200809@student.pg.edu.pl>
2025-11-26 19:53:35 +0100

    Swap mechanism between two state cameras, based on current 'game-state'

--- src/components/editor/CanvasController.tsx
+++ src/components/editor/CanvasController.tsx
@@ -1,17 +1,45 @@
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useThree } from '@react-three/fiber';
 import {
   GizmoHelper,
   GizmoViewport,
   Grid,
   OrbitControls,
+  PerspectiveCamera,
   TransformControls,
 } from '@react-three/drei';
 import ToolbarComponent from './Toolbar';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import EditingMode from '../../types/EditingMode';
 import { useEditorContext } from '../../hooks/useEditorContext';
 import useEditorKeys from '../../hooks/useEditorKeys';
 import StartStopBtnToolbar from './StartStopBtnToolbar';
+
+function SceneCameras({ running }: { running: boolean }) {
+  const editorCamRef = useRef(null);
+  const gameCamRef = useRef(null);
+  const { set } = useThree();
+
+  useEffect(() => {
+    if (running && gameCamRef.current) set({ camera: gameCamRef.current });
+    else if (!running && editorCamRef.current)
+      set({ camera: editorCamRef.current });
+  }, [running, set]);
+
+  return (
+    <>
+      <PerspectiveCamera
+        ref={editorCamRef}
+        makeDefault
+        position={[3, 2, -3]}
+      ></PerspectiveCamera>
+      <PerspectiveCamera
+        ref={gameCamRef}
+        makeDefault
+        position={[5, 5, 5]}
+      ></PerspectiveCamera>
+    </>
+  );
+}
 
 function CanvasController() {
   const [mode, selectMode] = useState<EditingMode>('translate');
@@ -23,13 +51,12 @@ function CanvasController() {
         <ToolbarComponent editingMode={mode} selectMode={selectMode} />
       )}
       <StartStopBtnToolbar />
-      <Canvas
-        className="canvas"
-        onPointerMissed={() => focus(null)}
-        camera={{ position: [3, 2, -3] }}
-      >
+      <Canvas className="canvas" onPointerMissed={() => focus(null)}>
         <ambientLight />
         <directionalLight position={[10, 10, 10]} />
+
+        <SceneCameras running={running}></SceneCameras>
+
         <OrbitControls makeDefault enableDamping={false} enabled={!running} />
         {focused && focused in sceneObjects && !running ? (
           <TransformControls object={sceneObjects[focused].ref} mode={mode} />
