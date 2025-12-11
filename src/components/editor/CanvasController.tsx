abb3437963e567a3cb8e708f961d4e009942a011
Szymon <s200809@student.pg.edu.pl>
2025-12-11 11:25:09 +0100

    swap mechanism

--- src/components/editor/CanvasController.tsx
+++ src/components/editor/CanvasController.tsx
@@ -10,6 +10,37 @@ import { useEditorContext } from '../../hooks/useEditorContext';
 import useEditorKeys from '../../hooks/useEditorKeys';
 import StartStopBtnToolbar from './StartStopBtnToolbar';
 import { RenderSystem } from '../../engine/systems/RenderSystem';
+import { useRef, useEffect } from 'react';
+import { useThree } from '@react-three/fiber';
+import { PerspectiveCamera } from '@react-three/drei';
+import { Scene } from 'three';
+
+function SceneCameras({ running }: { running: boolean }) {
+  const editorCamRef = useRef(null);
+  const gameCamRef = useRef(null);
+  const { set } = useThree();
+
+  useEffect(() => {
+    if (running && gameCamRef.current) set({ camera: gameCamRef.current });
+    else if (!running && editorCamRef.current)
+      set({ camera: editorCamRef.current });
+  }, [running, set]);
+
+  return (
+    <>
+      <PerspectiveCamera
+        ref={editorCamRef}
+        makeDefault
+        position={[3, 2, -3]}
+      ></PerspectiveCamera>
+      <PerspectiveCamera
+        ref={gameCamRef}
+        makeDefault
+        position={[5, 5, 5]}
+      ></PerspectiveCamera>
+    </>
+  );
+}
 
 function CanvasController() {
   const { running, focus } = useEditorContext();
@@ -26,6 +57,7 @@ function CanvasController() {
       >
         <ambientLight />
         <directionalLight position={[10, 10, 10]} />
+        <SceneCameras running={running} />
         <OrbitControls makeDefault enableDamping={false} enabled={!running} />
         <Grid sectionSize={2} infiniteGrid />
         {!running && (
