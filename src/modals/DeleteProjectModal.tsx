3dfd3a600fbe89930beb1a0497c229dc0137cf32
Szymon Mastalerz <33292538+simon04PL@users.noreply.github.com>
2025-12-21 20:06:38 +0100

    add delete project endpoint (#152)

---
+++ src/modals/DeleteProjectModal.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Modal from './ManageProjectModal';
+
+type DeleteProjectModalProps = {
+  open: boolean;
+  onClose: () => void;
+  projectName: string;
+  onConfirm: (e?: React.MouseEvent<HTMLElement>) => void;
+  deleting?: boolean;
+};
+
+export default function DeleteProjectModal({
+  open,
+  onClose,
+  projectName,
+  onConfirm,
+  deleting = false,
+}: DeleteProjectModalProps) {
+  return (
+    <Modal open={open} onClose={onClose} title="Delete project">
+      <p>Are you sure you want to delete "{projectName}"?</p>
+      <div className="modal-actions">
+        <button
+          type="button"
+          className="btn btn-secondary"
+          onClick={onClose}
+          disabled={deleting}
+        >
+          Cancel
+        </button>
+        <button
+          type="button"
+          className="btn btn-warning"
+          onClick={onConfirm}
+          disabled={deleting}
+        >
+          {deleting ? 'Deleting...' : 'Delete'}
+        </button>
+      </div>
+    </Modal>
+  );
+}
