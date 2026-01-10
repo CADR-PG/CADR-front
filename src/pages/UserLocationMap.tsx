40b6b6ec32c026f347abbc46a3d09cee7ee0351e
Szymon Mastalerz <33292538+simon04PL@users.noreply.github.com>
2026-01-10 17:06:45 +0100

    repair api

--- src/pages/UserLocationMap.tsx
+++ src/pages/UserLocationMap.tsx
@@ -44,7 +44,7 @@ export default function UserLocationMap() {
         <MapContainer key={`${center[0]},${center[1]}`} className="user-map">
           <TileLayer
             {...{
-              url: `https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic3p5bW9ucGciLCJhIjoiY21rNGF4NHRwMDVvejNjcjUzZXNrOTV4MCJ9.KgG1tdy6ToVuCHCBZ0Sf9Q`,
+              url: `https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}@2x?access_token=${import.meta.env.VITE_MAPBOX_TOKEN ?? ''}`,
               attribution:
                 '&copy; <a href="https://www.mapbox.com/">Mapbox</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
               tileSize: 512,
