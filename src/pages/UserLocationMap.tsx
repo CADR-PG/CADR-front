72e08287b74d3a694f0be0477216a76ca49c1d5d
Szymon Mastalerz <33292538+simon04PL@users.noreply.github.com>
2026-01-11 11:41:21 +0100

    add api key

--- src/pages/UserLocationMap.tsx
+++ src/pages/UserLocationMap.tsx
@@ -44,7 +44,7 @@ export default function UserLocationMap() {
         <MapContainer key={`${center[0]},${center[1]}`} className="user-map">
           <TileLayer
             {...{
-              url: `https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}@2x?access_token=${import.meta.env.VITE_MAPBOX_TOKEN ?? ''}`,
+              url: `https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic3p5bW9ucGciLCJhIjoiY21rOWxzMGc4MWJkcjNoc21ucnB2YmZ0eiJ9.hzcl3P-unEw6rMe4lmx8pw`,
               attribution:
                 '&copy; <a href="https://www.mapbox.com/">Mapbox</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
               tileSize: 512,
