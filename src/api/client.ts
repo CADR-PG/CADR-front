512b8443aecd6102730007a4bf0e54ad31973de5
Szymon Mastalerz <33292538+simon04PL@users.noreply.github.com>
2026-05-20 14:56:27 +0200

    change api domain

--- src/api/client.ts
+++ src/api/client.ts
@@ -10,7 +10,7 @@ import AddProjectData from '../types/AddProjectData';
 import SaveSceneData from '../types/SaveSceneData';
 
 const localApiUrl = '/api';
-const productionApiUrl = 'https://api.cadr.studio';
+const productionApiUrl = 'https://api.cadr.games';
 export const apiUrl = import.meta.env.DEV ? localApiUrl : productionApiUrl;
 
 export const apiClient = axios.create({
